60270b5a0171089a6f3db64a10e9b273402793d7
Saleem Jibril <saleemjibril5@gmail.com>
2022-05-31 02:23:32 +0100

    setup server and production build

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const configPort = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+
+app.get('*',(req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
+});
+
+app.listen(configPort, () => {
+    console.log('server is up')
+});
