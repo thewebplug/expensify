7d95da0140cacc04a3f14b9447187f073a8c7472
Saleem Jibril <saleemjibril5@gmail.com>
2022-06-25 14:31:31 +0100

    updated package.json and webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { webpack } = require('webpack');
+const webpack = require('webpack');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 if(process.env.NODE_ENV === 'test') {
